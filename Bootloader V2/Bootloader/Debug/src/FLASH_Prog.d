06828f515727d6f6979aff4ef3207cf9cfcdaba0
Abdelrahmanelneshwy <110162529+Abdelrahmanelneshwy@users.noreply.github.com>
2023-10-27 10:29:10 +0200

    update Boot loader V2
    increase new Future which make our Bootloader with two Banks to avoid missing of application incase of flashing corrupted data

---
+++ Bootloader V2/Bootloader/Debug/src/FLASH_Prog.d
@@ -0,0 +1,15 @@
+src/FLASH_Prog.o: ../src/FLASH_Prog.c ../src/../include/LIB/Bit_Math.h \
+ ../src/../include/LIB/STD_TYPES.h \
+ ../include/MCAL/FLASH/FLASH_Interface.h \
+ ../include/MCAL/FLASH/FLASH_Config.h \
+ ../include/MCAL/FLASH/FLASH_Private.h
+
+../src/../include/LIB/Bit_Math.h:
+
+../src/../include/LIB/STD_TYPES.h:
+
+../include/MCAL/FLASH/FLASH_Interface.h:
+
+../include/MCAL/FLASH/FLASH_Config.h:
+
+../include/MCAL/FLASH/FLASH_Private.h:
---
+++ Bootloader V2/Bootloader/Debug/src/HEX_PARSE.d
@@ -0,0 +1,17 @@
+src/HEX_PARSE.o: ../src/HEX_PARSE.c ../src/../include/LIB/Bit_Math.h \
+ ../src/../include/LIB/STD_TYPES.h ../include/App/HEX_Parser/HEX_PARSE.h \
+ ../include/MCAL/FLASH/FLASH_Interface.h \
+ ../include/MCAL/FLASH/FLASH_Config.h \
+ ../include/MCAL/FLASH/FLASH_Private.h
+
+../src/../include/LIB/Bit_Math.h:
+
+../src/../include/LIB/STD_TYPES.h:
+
+../include/App/HEX_Parser/HEX_PARSE.h:
+
+../include/MCAL/FLASH/FLASH_Interface.h:
+
+../include/MCAL/FLASH/FLASH_Config.h:
+
+../include/MCAL/FLASH/FLASH_Private.h:
