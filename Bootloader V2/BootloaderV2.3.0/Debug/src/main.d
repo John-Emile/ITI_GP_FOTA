8ba95df56f2c2d14e810d8cf0170bc9537eeb8d1
ayaahmedsamir <115669647+ayaahmedsamir@users.noreply.github.com>
2023-10-30 00:12:13 +0200

    Set the high address to be configurable

---
+++ Bootloader V2/BootloaderV2.3.0/Debug/src/main.d
@@ -0,0 +1,28 @@
+src/main.o: ../src/main.c ../src/../include/LIB/Bit_Math.h \
+ ../src/../include/LIB/STD_TYPES.h ../include/MCAL/RCC/MRCC_Interface.h \
+ ../include/LIB/STD_Types.h ../include/MCAL/GPIO/GPIO_interface.h \
+ ../include/MCAL/MSysTick/MSTK_Interface.h \
+ ../include/MCAL/USART/USART_interface.h \
+ ../include/MCAL/FLASH/FLASH_Interface.h \
+ ../include/MCAL/FLASH/FLASH_Private.h \
+ ../include/App/Hex\ Parser/HEX_PARSE.h
+
+../src/../include/LIB/Bit_Math.h:
+
+../src/../include/LIB/STD_TYPES.h:
+
+../include/MCAL/RCC/MRCC_Interface.h:
+
+../include/LIB/STD_Types.h:
+
+../include/MCAL/GPIO/GPIO_interface.h:
+
+../include/MCAL/MSysTick/MSTK_Interface.h:
+
+../include/MCAL/USART/USART_interface.h:
+
+../include/MCAL/FLASH/FLASH_Interface.h:
+
+../include/MCAL/FLASH/FLASH_Private.h:
+
+../include/App/Hex\ Parser/HEX_PARSE.h:
---
+++ Bootloader V2/BootloaderV2.3.0/Debug/system/src/cmsis/vectors_stm32f410Cx.d
@@ -0,0 +1,5 @@
+system/src/cmsis/vectors_stm32f410Cx.o: \
+ ../system/src/cmsis/vectors_stm32f410Cx.c \
+ ../system/include/cortexm/ExceptionHandlers.h
+
+../system/include/cortexm/ExceptionHandlers.h:
