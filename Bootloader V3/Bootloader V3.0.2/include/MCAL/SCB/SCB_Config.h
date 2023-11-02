7c6f6ca6548afe299bc09da05176d8b2aca2344f
Abdelrahmanelneshwy <110162529+Abdelrahmanelneshwy@users.noreply.github.com>
2023-11-02 15:01:01 +0200

    Update main comments

---
+++ Bootloader V3/Bootloader V3.0.2/include/MCAL/SCB/SCB_Config.h
@@ -0,0 +1,13 @@
+/*
+ * SCB_Config.h
+ *
+ *  Created on: Sep 11, 2023
+ *      Author: adham
+ */
+
+#ifndef MCAL_SCB_SCB_CONFIG_H_
+#define MCAL_SCB_SCB_CONFIG_H_
+
+
+
+#endif /* MCAL_SCB_SCB_CONFIG_H_ */
